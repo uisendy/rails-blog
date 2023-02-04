2c50d102bc5ce3eed841138b03acc7626bd12eb8
Inieke Sendy <ini.e.sendy@gmail.com>
2023-02-04 13:17:04 +0100

    [ft-process-data-model]: Update Schema data

--- db/schema.rb
+++ db/schema.rb
@@ -15,34 +15,25 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_02_172818) do
   enable_extension "plpgsql"
 
   create_table "comments", force: :cascade do |t|
-    t.integer "author"
-    t.integer "post"
     t.text "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "author_id", null: false
     t.bigint "post_id", null: false
-    t.index ["author"], name: "index_comments_on_author"
     t.index ["author_id"], name: "index_comments_on_author_id"
-    t.index ["post"], name: "index_comments_on_post"
     t.index ["post_id"], name: "index_comments_on_post_id"
   end
 
   create_table "likes", force: :cascade do |t|
-    t.integer "author"
-    t.integer "post"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "author_id", null: false
     t.bigint "post_id", null: false
-    t.index ["author"], name: "index_likes_on_author"
     t.index ["author_id"], name: "index_likes_on_author_id"
-    t.index ["post"], name: "index_likes_on_post"
     t.index ["post_id"], name: "index_likes_on_post_id"
   end
 
   create_table "posts", force: :cascade do |t|
-    t.integer "author"
     t.string "title"
     t.text "text"
     t.integer "comments_counter"
@@ -50,7 +41,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_02_172818) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "author_id", null: false
-    t.index ["author"], name: "index_posts_on_author"
     t.index ["author_id"], name: "index_posts_on_author_id"
   end
 
