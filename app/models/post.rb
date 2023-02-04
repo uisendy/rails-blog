cd2417fbeaff510184a2770666b2c2349151e9af
Inieke Sendy <ini.e.sendy@gmail.com>
2023-02-04 13:27:07 +0100

    [ft-process-data-model]: fix: linters

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
-  belongs_to :author, class_name: "User", foreign_key: "author_id"
-  has_many :likes, class_name: "Like", foreign_key: "post_id"
-  has_many :comments, class_name: "Comment", foreign_key: "post_id"
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+  has_many :likes, class_name: 'Like', foreign_key: 'post_id'
+  has_many :comments, class_name: 'Comment', foreign_key: 'post_id'
 
   def update_posts_count
     author.increment!(:posts_counter)
@@ -10,5 +10,4 @@ class Post < ApplicationRecord
   def recent_comments
     comments.limit(5).order(created_at: :asc)
   end
-
 end
