ca90357b439670d027dbd91abf24747b6a27b4a0
Inieke Sendy <ini.e.sendy@gmail.com>
2023-02-11 22:15:01 +0100

    [feature-forms]: Add: Methods for creating and reading froms

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,6 +4,24 @@ class PostsController < ApplicationController
     @posts = Post.where(author_id: params[:user_id])
   end
 
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(posts_params)
+    @post.author = current_user
+    if @post.save
+      redirect_to user_posts_path(id: @post.author_id)
+    else
+      render :new, alert: 'ERROR! something went wrong while creating the post'
+    end
+  end
+
+  def posts_params
+    params.require(:post).permit(:title, :text)
+  end
+
   def show
     @post = Post.find(params[:id])
   end
