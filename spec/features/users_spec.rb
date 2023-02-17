2ffcdff264b81adf4cf5f119c494e472f3aa5db5
Inieke Sendy <ini.e.sendy@gmail.com>
2023-02-17 08:39:18 +0100

    [feature-integration_test]: Add integration test to user show page

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -40,4 +40,44 @@ RSpec.feature 'Users', type: :feature do
     end
   end
 
+  describe 'user#show' do
+    before(:each) do
+      visit user_path(@user)
+    end
+    it "should show the user's profile picture" do
+      expect(page.body).to include('https://somepic.potraits/5158')
+    end
+    it 'should show username' do
+      expect(page).to have_content('David')
+    end
+
+    it 'should show the number of posts the user' do
+      expect(page).to have_content('Number of posts: 4')
+    end
+
+    it "should show the user's bio" do
+      expect(page).to have_content('Developer')
+    end
+
+    it "should show the user's first 3 posts" do
+      expect(page).to have_content('Hello, one')
+      expect(page).to have_content('Hello, two')
+      expect(page).to have_content('Hello, three')
+      expect(page).to have_no_content('Hello, four')
+    end
+
+    it 'should render see all posts button' do
+      expect(page).to have_content('See all posts')
+    end
+
+    it "should redirect to that post's show page when see all posts button is clicked" do
+      click_link('See all posts')
+      expect(page).to have_content('Pagination')
+    end
+
+    it 'should redirect to the user posts page' do
+      click_link(@first_post.title)
+      expect(current_path).to eq(user_post_path(@user, @first_post))
+    end
+  end
 end
